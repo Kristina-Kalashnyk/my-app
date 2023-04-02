833eb22873c591b449f4c06880daa28dc83f368e
Krystyna Kalashnyk <kristinakalashnyk17@gmail.com>
2023-04-02 21:18:52 +0300

    Added pics to DB

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,13 +11,16 @@ root.render(
   </React.StrictMode>
 );
 
+/*
+let productCard = document.querySelector(".products__card-one");
+   
+   if (productCard) {
+    
+      productCard.addEventListener('click', function () {
+         alert('dd');
+    })
+}*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-/*let productCard = document.querySelector(".products__card-one");
-productCard.addEventListener('click', function (e) {
-
-})
-*/
