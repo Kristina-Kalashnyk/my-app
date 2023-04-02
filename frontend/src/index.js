478c6a33f554aa32a66e2b430798dbb517cead4a
Krystyna Kalashnyk <kristinakalashnyk17@gmail.com>
2023-04-02 19:01:55 +0300

    Get some data from DB

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,3 +15,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+let productCard = document.querySelector(".products__card-one");
+productCard.addEventListener('click', function (e) {
+
+})
